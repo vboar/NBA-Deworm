dcaa14243ca212abf1a277a0a452758edda49290
vboar <dgboar@163.com>
2015-06-04 15:29:42 +0800

    实现了team的一个api。

--- NBA_Server/src/main/java/api/TeamAPI.java
+++ NBA_Server/src/main/java/api/TeamAPI.java
@@ -1,36 +1,88 @@
 package api;
 
+import dao.DaoFactory;
+import dao.TeamDao;
+import dao.impl.DaoFactoryImpl;
+import entity.TeamInfo;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import service.ServiceFactory;
+import service.TeamService;
+import service.impl.ServiceFactoryImpl;
+import vo.TeamInfoVO;
+
+import java.rmi.RemoteException;
+import java.util.List;
 
 /**
  * Created by Vboar on 2015/6/3.
  */
 public class TeamAPI {
 
+    private TeamService ts;
+
+    public TeamAPI() {
+        try {
+            ts = ServiceFactoryImpl.getInstance().getTeamService();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
     public String getStr(String str) {
         if (str.equals("/all") || str.equals("/all/")) {
             // command is /team/all or /team/all/
-            JSONObject json = new JSONObject();
-            JSONArray jsonMembers = new JSONArray();
-            JSONObject member1 = new JSONObject();
-            member1.put("loginname", "zhangfan");
-            member1.put("password", "userpass");
-            member1.put("email", "10371443@qq.com");
-            member1.put("sign_date", "2007-06-12");
-            jsonMembers.put(member1);
-
-            JSONObject member2 = new JSONObject();
-            member2.put("loginname", "zf");
-            member2.put("password", "userpass");
-            member2.put("email", "8223939@qq.com");
-            member2.put("sign_date", "2008-07-16");
-            jsonMembers.put(member2);
-            jsonMembers.put(member2);
-            jsonMembers.put(member2);
-            json.put("users", jsonMembers);
-            return json.toString();
+            return all();
         }
         return null;
+    }
+
+    private String all() {
+        try {
+            List<TeamInfoVO> list = ts.getAllTeamInfo();
+            JSONArray teams = new JSONArray();
+            for (TeamInfoVO vo: list) {
+                teams.put(getSimpleTeamInfoJO(vo));
+            }
+            return teams.toString();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 由VO获得球队简要信息的JO
+     * @param vo
+     * @return
+     */
+    private JSONObject getSimpleTeamInfoJO(TeamInfoVO vo) {
+        JSONObject jo = new JSONObject();
+        jo.put("name", vo.name);
+        jo.put("abbr", vo.abbr);
+        jo.put("buildup_time", vo.buildup_time);
+        jo.put("location", vo.location);
+        jo.put("division", vo.division);
+        jo.put("league", vo.league);
+        return jo;
+    }
+
+    /**
+     * 由VO获得球队信息的JO
+     * @param vo
+     * @return
+     */
+    private JSONObject getTeamInfoJO(TeamInfoVO vo) {
+        JSONObject jo = new JSONObject();
+        jo.put("name", vo.name);
+        jo.put("abbr", vo.abbr);
+        jo.put("buildup_time", vo.buildup_time);
+        jo.put("location", vo.location);
+        jo.put("division", vo.division);
+        jo.put("league", vo.league);
+        jo.put("record", vo.record);
+        jo.put("playeroff_appearance", vo.playeroff_appearance);
+        jo.put("championships", vo.championships);
+        return jo;
     }
 }
