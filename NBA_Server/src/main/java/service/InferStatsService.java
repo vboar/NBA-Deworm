a41cee3e065f851b6471cdf764704ad66e5aa9b0
JaneLdq <563870098@qq.com>
2015-06-15 13:02:35 +0800

    解决报错

---
+++ NBA_Server/src/main/java/service/InferStatsService.java
@@ -0,0 +1,42 @@
+package service;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import vo.MultiRegressionVO;
+import vo.SimpleRegressionVO;
+import vo.TeamWinAnalysisVO;
+
+public interface InferStatsService extends Remote{
+
+	/**
+	 * 获得单个赛季——球队主客场胜场影响差异分析结果
+	 * @param season 单个赛季
+	 * @return
+	 */
+	public TeamWinAnalysisVO getTeamTestingResultBySeason(String season) throws RemoteException;
+	
+	/**
+	 * 获得10个赛季——球队主客场胜场影响差异统计分析结果
+	 * @return
+	 */
+	public TeamWinAnalysisVO getTeamTestingResult_10() throws RemoteException;
+
+	/**
+	 * 获得一个赛季的球队数据的多元回归分析
+	 * @param season
+	 * @return
+	 * @throws RemoteException
+	 */
+	public MultiRegressionVO getMultiRegression(String season) throws RemoteException;
+
+	/**
+	 * 获得某个数据与球队净胜分的一元回归分析
+	 * @param typeNum
+	 * @param season
+	 * @return
+	 * @throws RemoteException
+	 */
+	public SimpleRegressionVO getSimpleRegression(int typeNum, String season) throws RemoteException;
+	
+}
---
+++ NBA_UI/src/main/java/service/impl/InferStatsServiceImpl.java
@@ -0,0 +1,45 @@
+package service.impl;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+import service.InferStatsService;
+import vo.MultiRegressionVO;
+import vo.SimpleRegressionVO;
+import vo.TeamWinAnalysisVO;
+
+public class InferStatsServiceImpl extends UnicastRemoteObject implements InferStatsService {
+	
+	public InferStatsServiceImpl() throws RemoteException {
+		super();
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public TeamWinAnalysisVO getTeamTestingResultBySeason(String season)
+			throws RemoteException {
+		return null;
+	}
+
+	@Override
+	public TeamWinAnalysisVO getTeamTestingResult_10() throws RemoteException {
+		return null;
+	}
+
+	@Override
+	public MultiRegressionVO getMultiRegression(String season)
+			throws RemoteException {
+		return null;
+	}
+
+	@Override
+	public SimpleRegressionVO getSimpleRegression(int typeNum, String season)
+			throws RemoteException {
+		return null;
+	}
+
+}
